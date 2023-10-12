8845046a1f6430387256316daa42efb7a275a177
lohoainam <96108318+lohoainam@users.noreply.github.com>
2023-10-12 18:32:09 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- BOYCODER || NHẬN CODE WEBSITE -->
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+    <meta charset="utf-8" />
+    <title>Lo Hoài Nam | Nam</title>
+    
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Lo Hoài Nam | Nam" />
+    <meta property="og:type" content="Lo Hoài Nam | Nam" />
+    <meta property="og:url" content="index.html" />
+    <meta property="og:image" content="../i.imgur.com/QdUScWg.jpg" />
+    <meta property="og:description" content="Lo Hoài Nam | Nam" />
+    <meta property="og:site_name" content="Lo Hoài Nam - About" />
+
+    <!-- Meta Data -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="format-detection" content="address=no"/>
+    <meta name="author" content="Lo Hoài Nam" />
+    <meta name="description" content="Lo Hoài Nam | Nam" />
+
+    <!-- Twitter data -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="FoxDev">
+    <meta name="twitter:title" content="FoxDev">
+    <meta name="twitter:description" content="FoxDev">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="/img/favicon.jpg" type="image/png">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="/assets/styles/style.css?k=1"/>
+    <link rel="stylesheet" type="text/css" href="/assets/demo/style-demo.css"/>
+  
+</head><body>
+    <!-- Preloader -->
+    <div class="preloader">
+	    <div class="preloader__wrap">
+		    <div class="circle-pulse">
+                <div class="circle-pulse__1"></div>
+                <div class="circle-pulse__2"></div>
+            </div>
+		    <div class="preloader__progress"><span></span></div>
+		</div>
+	</div>
+
+    <main class="main">
+	    <!-- Header Image -->
+		<div class="header-image">
+		    <div class="js-parallax" style="background-image: url(https://wallpapercave.com/wp/wp4676582.jpg);"></div>
+		</div>
+		
+	    <div class="container gutter-top">
+		    <!-- Header -->
+		    <header class="header box">
+			    <div class="header__left">
+				    <div class="header__photo">
+					    <img class="header__photo-img" src="/img/avt.jpg" alt="Lo Hoài Nam">
+					</div>
+					<div class="header__base-info">
+					    <h4 class="title titl--h4">Lo Hoài Nam</h4>
+						<div class="status">Developer - Software Engnieer - IT</div>
+					<ul class="header__social">
+						    <li><a href="https://www.facebook.com/Nam.Thich.An.Co"><i class="font-icon icon-facebook1"></i></a></li>
+						    <li><a href="https://twitter.com/"><i class="font-icon icon-twitter1"></i></a></li>
+						    <li><a href="https://www.instagram.com/bolalohoainam/"><i class="font-icon icon-instagram1"></i></a></li>
+						    <li><a href="https://github.com/lohoainam"><i class="font-icon icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="header__right">
+				    <ul class="header__contact">
+					    <li><span class="overhead">Email</span><a href="#" class="__cf_email__" >lohoainam37@gmail.com</a></li>
+						<li><span class="overhead">Phone</span>+84 (097) 2565612</li>
+					</ul>
+					<ul class="header__contact">
+					    <li><span class="overhead">Birthday</span>22 Nov, 2008</li>
+						<li><span class="overhead">Location</span>Nghệ An, Vietnam</li>
+					</ul>
+				</div>
+			</header>
+			
+		    <div class="row sticky-parent">
+			    <!-- Sidebar nav --><aside class="col-12 col-md-12 col-lg-2">
+				    <div class="sidebar box sticky-column">
+	                    <ul class="nav">
+                            <li class="nav__item"><a class="" href="/"><i class="icon-user"></i>Về tôi</a></li>
+		                    <li class="nav__item"><a class="" href="/Resume"><i class="icon-file-text"></i>Tóm tắt</a></li>
+		                    <li class="nav__item"><a class="active" href="/Project"><i class="icon-image"></i> Project</a></li>
+                            <!-- <li class="nav__item"><a class="" href="/Blog"><i class="icon-book-open"></i>Blog</a></li> -->
+                            <li class="nav__item"><a class="" href="/contact"><i class="icon-book"></i>Liên hệ</a></li>
+							<li class="nav__item"><a class="" href="/download"><i class="icon-search"></i>Khác</a></li>
+                        </ul>
+					</div>
+		        </aside>
+		        <!-- Content -->
+		        <div class="col-12 col-md-12 col-lg-10">
+				    <div class="box box-content">
+					    <div class="pb-2">
+		                    <h1 class="title title--h1 first-title title__separate">Project</h1>
+					    </div>
+						
+						<!-- Filter -->
+                        <div class="select">
+			                <span class="placeholder">Select category</span>
+			                <ul class="filter">
+			                    <li class="filter__item">Category</li>
+				                <li class="filter__item active" data-filter="*"><a class="filter__link active" href="#filter">All</a></li>
+				                <li class="filter__item" data-filter=".category-ShopGame"><a class="filter__link" href="#filter">Shop Nick Game</a></li>
+				                <li class="filter__item" data-filter=".category-CharCard"><a class="filter__link" href="#filter">Web gạch thẻ</a></li>
+				                <li class="filter__item" data-filter=".category-Fashion"><a class="filter__link" href="#filter">Web bán hàng</a></li>	
+		                </div>
+
+						<!-- Gallery -->
+				        <div class="gallery-grid js-grid-row js-filter-container">
+					        <div class="gutter-sizer"></div>
+					        <!-- Item 5 -->
+							<a href="/Project/Views-9.php">
+							<figure class="gallery-grid__item category-CharCard">
+								<div class="gallery-grid__image-wrap">
+									<img class="gallery-grid__image cover lazyload" src="https://i.imgur.com/T6i9b9w.jpg" data-zoom alt="100%" />
+								</div>
+                                <figcaption class="gallery-grid__caption">
+							        <h4 class="title title--h6 gallery-grid__title">[SELLCARD_V1] Source Code Bán thẻ cào điện thoại thẻ game các loại,... | Nam</h4>
+							        <span class="gallery-grid__category">11:26:26 26-06-2023</span>
+						        </figcaption>
+							</figure>
+							</a>
+					       
+						</div>
+					</div>
+          
+          <!-- Footer -->
+            </div>
+      </div>
+    </div>  
+     <footer class="footer">© 2023 Developed by <a href="https://www.facebook.com/Nam.Thich.An.Co">Nam</a></footer>
+    </main>
+
+
+    <!-- SVG masks -->
+    <svg class="svg-defs">
+        <clipPath id="avatar-box">
+            <path d="M1.85379 38.4859C2.9221 18.6653 18.6653 2.92275 38.4858 1.85453 56.0986.905299 77.2792 0 94 0c16.721 0 37.901.905299 55.514 1.85453 19.821 1.06822 35.564 16.81077 36.632 36.63137C187.095 56.0922 188 77.267 188 94c0 16.733-.905 37.908-1.854 55.514-1.068 19.821-16.811 35.563-36.632 36.631C131.901 187.095 110.721 188 94 188c-16.7208 0-37.9014-.905-55.5142-1.855-19.8205-1.068-35.5637-16.81-36.63201-36.631C.904831 131.908 0 110.733 0 94c0-16.733.904831-37.9078 1.85379-55.5141z"/>
+        </clipPath>
+        <clipPath id="avatar-hexagon">
+             <path d="M0 27.2891c0-4.6662 2.4889-8.976 6.52491-11.2986L31.308 1.72845c3.98-2.290382 8.8697-2.305446 12.8637-.03963l25.234 14.31558C73.4807 18.3162 76 22.6478 76 27.3426V56.684c0 4.6805-2.5041 9.0013-6.5597 11.3186L44.4317 82.2915c-3.9869 2.278-8.8765 2.278-12.8634 0L6.55974 68.0026C2.50414 65.6853 0 61.3645 0 56.684V27.2891z"/>
+        </clipPath>   
+    </svg>
+  
+    <div class="back-to-top"></div>
+
+    
+  
+  <!-- JavaScripts -->
+  <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="/assets/js/jquery-3.4.1.min.js"></script>
+  <script src="/assets/js/plugins.min.js"></script>
+    <script src="/assets/js/common.js"></script>
+  
+  <script src="/assets/demo/plugins-demo.js"></script>
+</body>
+</html>
